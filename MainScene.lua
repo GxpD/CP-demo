4719438246fb7d25564b914521c118282728a724
GxpD <850411098@qq.com>
2016-08-09 18:53:18 +0800

    test
    ...

---
+++ MainScene.lua
@@ -0,0 +1,72 @@
+require("Gplay")
+--require ("Cocos2d")
+--require ("Cocos2dConstants")
+
+local MainScene = class("MainScene", cc.load("mvc").ViewBase)
+
+function MainScene:onCreate()
+
+    gplay:initSDK(gplaydemo,gplaydemo,gplaydemo)
+
+    -- add background image
+    gplay:preloadGroup("mainScene",function ()
+        -- body
+        display.newSprite("he.jpg")
+        :move(display.center)
+        :addTo(self)
+    display.newSprite("HelloWorld.png")
+        :move(display.cx-200,display.cy)
+        :addTo(self)
+    display.newSprite("HelloWorld.png")
+        :move(display.cx+200,display.cy)
+        :addTo(self)
+   local bool=gplay:isInGplayEnv()
+
+   cc.Label:createWithSystemFont("GP环境:"..tostring(bool), "Arial", 40)
+        :move(display.cx-250, display.cy + 200)
+        :addTo(self)
+local x = gplay:getNetworkType()
+cc.Label:createWithSystemFont("网络类型:"..tostring(x), "Arial", 40)
+        :move(display.cx+250, display.cy + 200)
+        :addTo(self)
+
+        cc.Label:createWithSystemFont("Hello World", "Arial", 40)
+        :move(display.cx, display.cy + 200)
+        :addTo(self)
+     local button = cc.MenuItemFont:create("跳转")
+                                   :setPosition(display.cx-550, display.cy - 550)
+
+     --local dir = cc.Director:getInstance()
+     local function clicklisent(sender)
+        -- body
+        gplay:backFromGroup("mainScene")
+        local twoSnece =require("app/views/Two")
+        local snece = twoSnece.create()
+        if snece then
+        cc.Director:getInstance():replaceScene(snece)
+        end
+        release_print("touch clicklisent")
+     end
+
+     button:registerScriptTapHandler(clicklisent)
+     local mn = cc.Menu:create(button)
+                       --:setPosition(display.cx, display.cy - 400)
+                        :addTo(self)
+
+    end)
+    
+   --[[cc.Label:createWithSystemFont("NO_NET:"..tostring(gplay.NetworkType.NO_NETWORK), "Arial", 40)
+        :move(display.cx+200, display.cy + 100)
+        :addTo(self)
+   cc.Label:createWithSystemFont("MOBILE:"..tostring(gplay.NetworkType.MOBILE), "Arial", 40)
+        :move(display.cx+400, display.cy + 100)
+        :addTo(self)
+    cc.Label:createWithSystemFont("WIFI:"..tostring(gplay.NetworkType.WIFI), "Arial", 40)
+        :move(display.cx+450, display.cy+50 )
+        :addTo(self) ]]
+    -- add HelloWorld label
+    
+
+end
+
+return MainScene
